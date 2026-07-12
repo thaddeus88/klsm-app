dfcefcd2b23ab54f047810e951f3405e80ffd1d9
thaddeus88 <thaddeus.chens@gmail.com>
2026-07-12 09:19:08 +0800

    Fixed missing firebase db export

--- src/firebase.js
+++ src/firebase.js
@@ -1,11 +1,7 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyCg7JF2MVE76XmTe78YohYL528-myxmUcw",
   authDomain: "klsm-workplace-inspection-hub.firebaseapp.com",
@@ -18,4 +14,7 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+
+// THESE TWO LINES ARE THE FIX:
+export const db = getFirestore(app);
+export const auth = getAuth(app);
